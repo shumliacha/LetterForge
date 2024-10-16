7b336f83ab87c6bf9de64ad269b4df246536d7c4
shumliacha <126417371+shumliacha@users.noreply.github.com>
2024-10-16 17:46:13 +0200

    Initial project

---
+++ LetterForge/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  LetterForge
+//
+//  Created by Kristina Litvinova on 16.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var word: UILabel!
+    
+    @IBOutlet weak var score: UILabel!
+    
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
